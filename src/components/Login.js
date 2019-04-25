b5355fc7e24e86fc996750dca31d8da930763e2a
Bryan Hawkins <bhawkins3729@gmail.com>
2019-04-25 10:51:15 -0500

    second commit

---
+++ src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleUsername = (e) => {
+        this.setState({ username: e.target.value })
+    }
+
+    handlePassword = (e) => {
+        this.setState({ password: e.target.value })
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                Login
+                <br />
+                <input
+                    onChange={this.handleUsername}
+                    placeholder="Username"
+                />
+                <br />
+                <input
+                    type='password'
+                    onChange={this.handlePassword}
+                    placeholder="Password"
+                />
+                <h3>Don't have an account? <Link to='/register'>Register today!</Link></h3>
+            </div>
+        )
+    }
+}
+
+export default Login
---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+
+export default (
+    <Switch>
+        <Route path='/register' component={Register} />
+        <Route exact path='/' component={Login} />
+        <Route path='/' render={() => <h1>Not Found</h1>} />
+    </Switch>
+)
